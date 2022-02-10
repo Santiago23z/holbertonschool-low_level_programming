f7771afbf26784786b4ee21bd6d9e8541339bca7
Santiago23z <santizapataalvares@gmail.com>
2022-02-10 13:30:34 -0800

    4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
 
-int main(void)
+/**
+ * main 
+ *
+ * Return: Always 0 (Success)
+ */
+ int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
-}        
+}
