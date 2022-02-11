b466c78115459e82ff4dae5bddb2fba1013ea1f1
Santiago23z <santizapataalvares@gmail.com>
2022-02-11 09:53:44 -0800

    0

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -3,8 +3,8 @@
 #include <stdio.h>
 
 /**
- * main - init
- * Return: Always 0
+ * main - escribir número aleatorio positivo negativo o cero
+ * Return: siempre 0
 */
 
 int main(void)
@@ -12,14 +12,14 @@ int main(void)
 int n;
 
 srand(time(0));
-n = rand() - RAND_MAX / 2;
-	
+	n = rand() - RAND_MAX / 2;
+
 if (n > 0)
-printf("%d is positive \n", n);
+printf("%d is positive\n", n);
 else if (n < 0)
-printf("%d is negative \n", n);
+printf("%d is negative\n", n);
 else
-printf("%d is zero \n", n);
-	
+printf("%d is zero\n", n);
+
 return (0);
 }
