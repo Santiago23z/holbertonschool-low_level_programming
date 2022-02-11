49c80a91f1439904c0db52aa47bcde3b5f4155ad
Santiago23z <santizapataalvares@gmail.com>
2022-02-11 13:30:22 -0800

    3

---
+++ 3-print_alphabets.c
@@ -0,0 +1,20 @@
+i#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - alafbet
+ *
+ * Return: Always 0 
+ */
+int main(void)
+{
+	char alf;
+
+	for (alf = 'a'; alf <= 'z'; alf++)
+		putchar(low);
+	for (alf = 'A'; alf <= 'Z'; alf++)
+		putchar(alf);
+	putchar('\n');
+
+	return (0);
+}
