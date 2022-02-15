6a8ec37327e67e3820ce5dc066612225c273f101
Santiago23z <santizapataalvares@gmail.com>
2022-02-15 11:52:08 -0800

    d

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,25 @@
+#include "main"
+
+/**
+ * jack_bauer - hora 24
+ *
+ * Return: Always 0.
+ */
+
+void jack_bauer(void)
+{
+	int hora;
+	int min;
+
+	for (hora = 0; hora < 24; hora++)
+	for (min = 0; min < 60; min++)
+	{
+		_putchar((hora / 10) + '0');
+		_putchar((hora % 10) + '0');
+		_putchar(':');
+		_putchar((min / 10) + '0');
+		_putchar((min % 10) + '0');
+		_putchar('\n');
+	}
+	
+}
