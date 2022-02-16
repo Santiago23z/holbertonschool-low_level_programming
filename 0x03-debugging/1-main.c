6533ed92aff7cb1f75a1dc84604f1ca0bc9afe48
Santiago23z <santizapataalvares@gmail.com>
2022-02-16 10:58:04 -0800

    s

---
+++ 0x03-debugging/1-main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+* main - causes an infinite loop
+* Return: 0
+*/
+
+int main(void)
+{
+        int i;
+
+        printf("don’t delete it! :(\n");
+
+        i = 0;
+
+        while (i < 10)
+        {
+                putchar(i);
+        }
+
+        printf("don’t delete it!! \\o/\n");
+
+        return (0);
+}
