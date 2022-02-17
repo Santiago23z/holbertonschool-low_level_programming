bdec3e03c89e798240bd827c15ddad1003e848a1
Santiago23z <santizapataalvares@gmail.com>
2022-02-17 11:53:58 -0800

    print most numbers

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - imprimir del 1 al 9 sin 2 y 4
+ * Return: 0
+ */
+void print_most_numbers(void)
+{
+	int a;
+
+	for (a = '0'; a <= '9'; a++)
+	{
+		if (a != 2 || a != 4)
+		{
+			_putchar(a);
+		}
+	_putchar('\n');
+	}
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -9,4 +9,5 @@ int _putchar(char c);
 int _isdigit(int c);
 int mul(int a, int b);
 void print_numbers(void);
+void print_most_numbers(void);
 #endif 
