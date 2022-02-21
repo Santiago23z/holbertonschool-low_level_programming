74d2afbe74293dea301185ad48d8f51ea4026d70
santiago23z <santizapataalvares@gmail.com>
2022-02-21 13:09:28 -0500

    0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "main.h"
+ /**
+  * reset_to_98 - funcion que tome int y apunte al parametro 98
+  * @n: variable
+  * Return: n
+  */
+
+ void reset_to_98(int *n);
+ {
+     *n = 98;
+ }
