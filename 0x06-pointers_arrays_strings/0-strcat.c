1a030001d99bfee04f063be09c3545f1334d5c93
santiago23z <santizapataalvares@gmail.com>
2022-02-23 20:22:57 +0000

    0

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _strcat - una funcion que concatene dos cadenas
+ * @dest: cadena 1
+ * @src: cadena 2
+ * Return: combinar dos cadenas
+ */
+char *_strcat(char *dest, char *src)
+{
+	int i, n;
+
+	for (i = 0; dest[i] != '\0'; i++)
+	{
+	}
+
+	for (n = 0; (dest[i + n] = *src++) != '\0'; n++)
+	{
+	}
+
+	return (dest);
