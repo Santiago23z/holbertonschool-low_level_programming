42862ed06fe1a7ce40e94aec52d1d6ce7929a351
santiago23z <santizapataalvares@gmail.com>
2022-02-24 15:36:31 +0000

    2

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -9,16 +9,16 @@
  */
 char *_strncpy(char *dest, char *src, int n)
 {
-	int i;
+int i;
 
-	for (i = 0; src[i] != '\0' && (i < n); i++)
-    {
-        dest[i] = src[i];
-    }
-		while (i < n)
-        {
-            dest[i] = '\0';
-            i++;
-        }
-	return (dest);
+for (i = 0; src[i] != '\0' && (i < n); i++)
+{
+dest[i] = src[i];
+}
+while (i < n)
+{
+dest[i] = '\0';
+i++;
+}
+return (dest);
 }
