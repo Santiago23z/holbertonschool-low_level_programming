ca4385a896bd3a461b84e753579d73b1417a4e69
santiago23z <santizapataalvares@gmail.com>
2022-02-24 15:01:27 +0000

    2

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,20 +1,20 @@
 #include "main.h"
 
 /**
- * _strcat - una funcion que concatene dos cadenas
+ * *_strncat - una funcion que concatene dos cadenas
  * @dest: cadena 1
  * @src: cadena 2
+ * @n: variable 3
  * Return: combinar dos cadenas
  */
 char *_strncat(char *dest, char *src, int n)
 {
 	int i, g;
-    
 
 	for (i = 0; dest[i] != '\0'; i++)
 	{
 	}
-	for (g = 0; ((dest[i + g] = *src++) != '\0') && (g < n-1); g++)
+	for (g = 0; ((dest[i + g] = *src++) != '\0') && (g < n - 1); g++)
 	{
 	}
 	return (dest);
