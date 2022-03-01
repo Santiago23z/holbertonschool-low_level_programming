34402c91608bda47aec5ece8348a59b35415227d
santiago23z <santizapataalvares@gmail.com>
2022-03-01 01:50:06 +0000

    2

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -8,13 +8,11 @@
  */
 char *_strchr(char *s, char c)
 {
-	int i;
-
-	for (i = 0; s[i] != '\0'; i++)
-	{
-		if (s[i] == c)
-		return (s + i);
-	}
-
-	return (0);
+int u;
+for (u = 0; s[u] != '\0'; u++)
+{
+if (s[u] == c)
+return (&s[u]);
+}
+return ('\0');
 }
