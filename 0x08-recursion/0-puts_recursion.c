e7baaf964ed6950b8fc351f9870c27eefcfd75bc
santiago23z <santizapataalvares@gmail.com>
2022-03-02 19:19:17 +0000

    commit 0

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -2,18 +2,18 @@
 
 /**
 * _puts_recursion - funcion que imprima una cadena
-* @s: variable 
+* @s: variable
 * Return: 0
 */
 void _puts_recursion(char *s)
 {
-    if (*s)
-    {
-        _putchar(*s);
-        _puts_recursion(s + 1);
-    }
-    else 
-    {
-        _putchar('\n');
-    }   
+if (*s)
+{
+_putchar(*s);
+_puts_recursion(s + 1);
+}
+else
+{
+_putchar('\n');
+}
 }
