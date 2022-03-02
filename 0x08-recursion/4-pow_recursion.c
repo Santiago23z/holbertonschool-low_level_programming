553500d32e513f955dc647431162c5cbf15a6775
santiago23z <santizapataalvares@gmail.com>
2022-03-02 21:59:06 +0000

    commit 5

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+* _pow_recursion - funcion que devuelva x a la y
+* @x: variable base
+* @y: variable exponente
+* Return: 0
+*/
+int _pow_recursion(int x, int y)
+{
+if (y < 0)
+return (-1);
+if (y == 0)
+return (1);
+else
+return (x * _pow_recursion(x, y - 1));
+}
