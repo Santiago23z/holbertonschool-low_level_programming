b244514d503606835edf654479da38cb3f03a18d
santiago23z <santizapataalvares@gmail.com>
2022-03-04 19:39:09 +0000

    s

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - prints the number of arguments passed into it
+ * @argc: number arguments
+ * @argv: array arguments
+ *
+ * Return: Always 0
+ */
+int main(int argc, char __attribute__((unused)) *argv[])
+{
+printf("%d\n", --argc);
+
+return (0);
+}
