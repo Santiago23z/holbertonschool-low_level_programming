ac846c025c7b1681ce3817c53e7c329e48d0a642
santiago23z <santizapataalvares@gmail.com>
2022-03-04 19:10:20 +0000

    dd

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+/**
+ * main - init
+ *@argc: count of arguments
+ *@argv: array conter
+ * Return: Always 0.
+ */
+
+int main(int argc, __attribute__((unused)) char *argv[])
+{
+int i;
+
+for (i = 0; i < argc; i++)
+{
+
+}
+printf("%d\n", i - 1);
+return (0);
+}
