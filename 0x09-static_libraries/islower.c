2004bd27a992223d60a3c4b1d11699e3deca9a8d
santiago23z <santizapataalvares@gmail.com>
2022-03-07 15:31:26 +0000

    s

--- 0x09-static_libraries/islower.c
+++ 0x09-static_libraries/islower.c
@@ -1,1 +1,15 @@
 int _islower(int c);
+#include "main.h"
+
+/**
+ * _islower - main
+ * @c: variable
+ * Return: 1
+ */
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+		return (1);
+	else
+		return (0);
+}
