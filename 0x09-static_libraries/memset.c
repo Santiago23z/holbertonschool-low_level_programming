9883edc4151dd61adb3ce1c3cc912528d76e28b9
santiago23z <santizapataalvares@gmail.com>
2022-03-07 15:34:07 +0000

    d

--- 0x09-static_libraries/memset.c
+++ 0x09-static_libraries/memset.c
@@ -1,1 +1,20 @@
-char *_memset(char *s, char b, unsigned int n);
+#include "main.h"
+/**
+ * _memset - llenar memoria con un byte
+ * @s: pointer
+ * @b: constant byte
+ * @n: n bytes
+ * Return: s
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int a = 0;
+
+	while (a < n)
+	{
+		s[a] = b;
+		a++;
+	}
+	return (s);
+}
+
