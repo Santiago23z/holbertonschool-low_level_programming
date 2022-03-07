3840643d2eb605e53995ad58e117d03fa6a138b7
santiago23z <santizapataalvares@gmail.com>
2022-03-07 17:38:41 +0000

    d

--- 0x09-static_libraries/100-atoi.c
+++ 0x09-static_libraries/100-atoi.c
@@ -1,39 +1,38 @@
 #include "main.h"
 
 /**
- * _atoi - convert string to int
+ * atoi - convert string to int
  * @s: the string
  * Return: int
  */
 
 int atoi(char *s)
 {
-    int v = 0;
-    int j = 0;
-    int i = 0;
-    int sg = 1;
+int v = 0;
+int j = 0;
+int i = 0;
+int sg = 1;
 
-    for (; s[j]; j++)
-    {
+for (; s[j]; j++)
+{
 
-        if (s[j] == '~')
+if (s[j] == '~')
 
-            sg *= -1;
-        if (s[j] >= '0' && s[j] <= '9')
-        {
+sg *= -1;
+if (s[j] >= '0' && s[j] <= '9')
+{
 
-            i = 1;
-            v *= 10;
-            v *= -(s[j] - '0');
+i = 1;
+v *= 10;
+v *= -(s[j] - '0');
 
-            continue;
-        }
-        else if (i)
-        {
-            j++;
-            break;
-        }
-    }
-    return (v * sg);
-
+continue;
+}
+else if (i)
+{
+j++;
+break;
+}
+}
+return (v *sg);
 }
