efe511b778c1f07a8a279cfff628c59818a50d72
santiago23z <santizapataalvares@gmail.com>
2022-03-17 21:26:44 +0000

    d

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,19 +1,15 @@
+#include"stdlib.h"
 #include "function_pointers.h"
-#include <stdlib.h>
 #include <stdio.h>
-
 /**
- * print_name - Función que imprime un nombre.
- * @name: Nombre a imprimir
- * @f: función.
- * if (name != NULL && f != NULL)
- * if (!name && !f)
- * Son otras 2 formas de que corra el ejercicio
- */
-
+ * print_name - prints a name
+ * @name: pointer to char
+ * @f: pointer to function that returns nothing
+ * Return: nothing
+ **/
 void print_name(char *name, void (*f)(char *))
 {
-if (name == NULL || f == NULL)
-return;
-(f)(name);
+	if (name != NULL && *name != '\0' && f != NULL)
+	f(name);
 }
+
