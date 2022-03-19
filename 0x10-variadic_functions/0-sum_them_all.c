248130ba2f18cc65e801f81c66c28ab08397ccf9
santiago23z <santizapataalvares@gmail.com>
2022-03-19 00:00:28 +0000

    s

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -1,27 +1,18 @@
 #include "variadic_functions.h"
 #include <stdio.h>
-
 /**
- * sum_them_all - Sums all given parameters
- * @n: Number to sum
+ * sum_them_all - this is main funtion
+ * @n: this is n
  * Return: the sum of parameters
  */
-
 int sum_them_all(const unsigned int n, ...)
 {
+unsigned int c;
+int sum = 0;
 va_list l;
-unsigned int sum, i;
 
-if (n == 0)
-{
-return (0);
-}
-sum = 0;
 va_start(l, n);
-for (i = 0; i < n; i++)
-{
+for (c = 0; c < n; c++)
 sum += va_arg(l, int);
-}
 va_end(l);
 return (sum);
-}
--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,17 +1,12 @@
-#ifndef VARIADIC_FUNCTIONS_H
-#define VARIADIC_FUNCTIONS_H
+#ifndef MAIN_H
+#define MAIN_H
 
-#include <stdarg.h>
 #include <stdio.h>
+#include <stdarg.h>
 
-int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
-void print_a_char(char *separator, va_list args);
-void print_a_integer(char *separator, va_list args);
-void print_a_float(char *separator, va_list args);
-void print_a_char_ptr(char *separator, va_list args);
-
+int _putchar(char c);
 #endif
