e0865a19f7440b967f2d5ab66a7f5f9034137c63
santiago23z <santizapataalvares@gmail.com>
2022-03-30 19:20:14 +0000

    avanced

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * Antes - imprima antes de la funcion principal
+ */
+void __attribute__ ((constructor)) Antes()
+{
+printf("You're beat! and yet, you must allow\n");
+printf("I bore my house upon my back!\n");
+}
