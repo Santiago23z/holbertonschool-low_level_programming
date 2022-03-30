b9f45fcf8d7cc1004395af47d389a151ab829bab
santiago23z <santizapataalvares@gmail.com>
2022-03-30 03:51:15 +0000

    s

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+#include <string.h>
+
+/**
+ * *add_node - Adds a new node at the beginning of a list_t list.
+ * @head: Beginning of a list_l
+ * @str: String to be duplicated
+ * Return: The address of the new element, or NULL if it failed
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *new;
+
+	new = malloc(sizeof(list_t));
+	if (new == NULL)
+		return (NULL);
+	new->str = strdup(str);
+	new->len = strlen(str);
+	new->next = *head;
+	*head = new;
+	return (new);
+}
---
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,40 @@
+#include "lists.h"
+#include <string.h>
+
+/**
+ * add_node_end - function that adds a new node at the end of a linked list
+ * @head: previus node head
+ * @str: string of the new node
+ *
+ * Return: addres of the new node
+ */
+list_t *add_node_end(list_t **head, const char *str)
+{
+	list_t *new;
+	list_t *current_node;
+
+	if (head == NULL)
+	{
+		return (NULL);
+	}
+	new = malloc(sizeof(list_t));
+	if (new == NULL)
+	{
+		return (NULL);
+	}
+	new->str = strdup(str);
+	new->len = strlen(str);
+	new->next = NULL;
+	if (*head == NULL)
+	{
+		*head = new;
+		return (new);
+	}
+	current_node = *head;
+	while (current_node->next != NULL)
+	{
+		current_node = current_node->next;
+	}
+	current_node->next = new;
+	return (new);
+}
