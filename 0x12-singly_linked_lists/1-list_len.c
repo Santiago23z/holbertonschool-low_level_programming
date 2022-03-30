9ed55683f3bfa1d21b78ccc51ecf05b02d323cab
santiago23z <santizapataalvares@gmail.com>
2022-03-30 15:24:43 +0000

    d

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,21 +1,20 @@
 #include "lists.h"
 /**
- * list_len - a function that returns the number
- *  of elements in a linked list_t list
- * @h: input
+ * list_len - function that returns the number
+ * of elements in a linked list_t list
+ * @h: entrada
  * Return: 0
  */
 size_t list_len(const list_t *h)
 {
-	int count = 0;
+int count = 0;
 
-	if (h == NULL)
-		return (0);
-
-	while (h != NULL)
-	{
-		count++;
-		h = h->next;
-	}
-	return (count);
+if (h == NULL)
+return (0);
+while (h != NULL)
+{
+count++;
+h = h->next;
+}
+return (count);
 }
