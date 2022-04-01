dbc6d72d3828276d8b9a599a24cb98454d9012fc
santiago23z <santizapataalvares@gmail.com>
2022-04-01 14:58:02 +0000

    n

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,11 +1,11 @@
 #include "lists.h"
 
-
 /**
  * print_listint - Prints all the elements of a listint_t list.
  * @h: First node
  * Return: The number of nodes
  */
+
 size_t print_listint(const listint_t *h)
 {
 	size_t counter = 0;
