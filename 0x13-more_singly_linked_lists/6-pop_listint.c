43ab4cebb80e6af29ec9b8430698c7aaee7bb1ce
santiago23z <santizapataalvares@gmail.com>
2022-04-01 14:34:23 -0400

    g

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * pop_listint - delete the node
+ * @head: cabecera, first node
+ * @e: variable
+ * Return: 0
+ */
+int pop_listint(listint_t **head)
+{
+listint_t *new = (*head)->next;
+int e;
+
+if (*head == NULL)
+return (0);
+e = (*head)->n;
+free(*head);
+*head = new;
+
+return (e);
+}
