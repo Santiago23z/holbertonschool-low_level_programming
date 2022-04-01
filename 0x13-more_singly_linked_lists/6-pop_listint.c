0293029010ac79d897e68055b3b8f341305ab76c
santiago23z <santizapataalvares@gmail.com>
2022-04-01 14:38:17 -0400

    g

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -3,7 +3,6 @@
 /**
  * pop_listint - delete the node
  * @head: cabecera, first node
- * @e: variable
  * Return: 0
  */
 int pop_listint(listint_t **head)
@@ -11,7 +10,7 @@ int pop_listint(listint_t **head)
 listint_t *new = (*head)->next;
 int e;
 
-if (*head == NULL)
+if (!head)
 return (0);
 e = (*head)->n;
 free(*head);
