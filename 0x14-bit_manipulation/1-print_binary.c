ac7d84dccd97b9be02854fe65a2f67cbfa0114fc
santiago23z <santizapataalvares@gmail.com>
2022-04-04 18:35:20 -0400

    second commit

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_binary - function that prints the binary representation of a number
+ * @n: variable
+ * Return: nothing
+ */
+void print_binary(unsigned long int n)
+{
+if (n >> 0)
+{
+if (n >> 1)
+print_binary(n >> 1);
+putchar((n & 1) + '0');
+}
+else
+{
+putchar('0');
+}
+}
