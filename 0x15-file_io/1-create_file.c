47cb000b336bc1429d1405c97be27c5ed1094677
santiago23z <santizapataalvares@gmail.com>
2022-04-05 17:09:54 -0400

    task 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -11,7 +11,7 @@ int create_file(const char *filename, char *text_content)
 
 	if (filename == NULL)
 		return (-1);
-	fileopen = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
+	fileopen = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 6000);
 	if (fileopen == -1)
 		return (-1);
 	if (text_content == NULL)
