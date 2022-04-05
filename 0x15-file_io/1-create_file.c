4139a0eb0186fbb73f42c774ad2e3eec2d861526
santiago23z <santizapataalvares@gmail.com>
2022-04-05 17:06:15 -0400

    task 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -7,24 +7,23 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-int flopen;
-int flwri;
-int i;
+	int fileopen, filewrite, i;
 
-if (filename == NULL)
-return (-1);
-flopen = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
-if (flopen == -1)
-return (-1);
-if (text_content == NULL)
-{
-close(flopen);
-return (1);
-}
-for (i = 0; text_content[i] != '\0'; i++)
-flwri = write(flopen, text_content, i);
-if (flwri == -1)
-return (-1);
-close(flopen);
-return (1);
+	if (filename == NULL)
+		return (-1);
+	fileopen = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
+	if (fileopen == -1)
+		return (-1);
+	if (text_content == NULL)
+	{
+		close(fileopen);
+		return (1);
+	}
+	for (i = 0; text_content[i] != '\0'; i++)
+		;
+	filewrite = write(fileopen, text_content, i);
+	if (filewrite == -1)
+		return (-1);
+	close(fileopen);
+	return (1);
 }
