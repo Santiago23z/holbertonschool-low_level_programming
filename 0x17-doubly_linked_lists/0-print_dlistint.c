124eecd73e4abe2df0e772905426d2ff43f46502
Santiago23z <98781406+Santiago23z@users.noreply.github.com>
2022-05-12 11:44:42 -0500

    s

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -11,9 +11,9 @@ size_t print_dlistint(const dlistint_t *h)
 {
 	size_t counter = 0;
 	
-	while (h)
+	while (h != NULL)
 	{
-		printf("%i\n", h->n);
+		printf("%d\n", h->n);
 		counter++;
 		h = h->next;
 	}
