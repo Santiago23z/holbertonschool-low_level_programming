28971d9a26fdccdf88b4997919157ecc99076e15
Santiago23z <98781406+Santiago23z@users.noreply.github.com>
2022-06-06 10:05:11 -0500

    x

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -9,14 +9,14 @@ hash_table_t *hash_table_create(unsigned long int size)
 {
 hash_table_t *table = NULL;
 
-if (table == NULL)
+if (size == NULL)
 return (NULL);
 table = malloc(sizeof(hash_table_t));
 if (!table)
 return (NULL);
 
 table->size = size;
-table->array = malloc(sizeof(hash_node_t) * size);
+table->array = malloc(sizeof(table) * size);
 if (!table->array)
 return (NULL);
 return (table);
