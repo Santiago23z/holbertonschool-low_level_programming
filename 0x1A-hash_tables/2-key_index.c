3c4b3fc7e67f14a58d9ad7dbf1b05b94b43bb508
Santiago23z <98781406+Santiago23z@users.noreply.github.com>
2022-06-06 10:13:36 -0500

    s

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -2,11 +2,11 @@
 #include "hash_tables.h"
 /**
  * key_index - function that gives you the index of a key.
- * @long_int: unsigned
- * @char: unsigned const
+ * @size: unsigned
+ * @key: unsigned const
  * Return: usnigned long
  */
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
-    return (hash_djb2(key) % size);
+return (hash_djb2(key) % size);
 }
